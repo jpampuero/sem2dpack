afa318421436d66825a23c3a0ecb7f18ea61f438
chaovite <chao.liang@geoazur.unice.fr>
2020-09-09 13:43:11 +0200

    add test for kinflt mode3, minor comment changes

--- EXAMPLES/TestStatic/KinfltModeIII/analyze_test.m
+++ EXAMPLES/TestStatic/KinfltModeIII/analyze_test.m
@@ -1,4 +1,4 @@
-% plot kinematic bc test
+% plot kinematic fault test mode 3
 
 % read grid and plot grid
 wdir = pwd;
@@ -23,18 +23,18 @@ it = 4;
 fault_z = 0;
 
 % obtain fault shear stress change from stresses
-dx = sem2d_snapshot_read('dx', it, wdir);
-s12 = sem2d_snapshot_read('s12', it , wdir);
-s22 = sem2d_snapshot_read('s22', it , wdir);
+dx = sem2d_snapshot_read('dy', it, wdir);
+s11 = sem2d_snapshot_read('s23', it , wdir);
 
+%%
 figure(1)
 subplot(1, 2, 1);
 sem2d_snapshot_plot(dx, grid, [-2, 2]*1e-4);
-title('ux');
+title('uy');
 set(gca,'fontsize',14)
 subplot(1,2, 2);
-wrk = sem2d_snapshot_plot(s12, grid, [-2, 2]*1e-4);
-title('sxz');
+wrk = sem2d_snapshot_plot(s11, grid, [-2, 2]*1e-4);
+title('syz');
 set(gca,'fontsize',14)
 % subplot(1,2, 3);
 % wrk = sem2d_snapshot_plot(s22, grid, [-2, 2]*1e-4);
@@ -49,16 +49,16 @@ set(gca,'fontsize',16)
 xlabel('x');
 ylabel('slip');
 
-s12 = s12(:);
+s11 = s11(:);
 flt_ind   = find(abs(wrk.coord(:,2))<1e-6);
 xz_flt    = wrk.coord(flt_ind, :);
-s12_flt  = s12(flt_ind);
+s11_flt  = s11(flt_ind);
 
 % obtain unique x index
 [x,IA,~] = unique(xz_flt(:, 1));
-s12_flt = s12_flt(IA);
+s11_flt = s11_flt(IA);
 subplot(2,1,2);
-plot(x, s12_flt,'linew',1.5);
+plot(x, s11_flt,'linew',1.5);
 set(gca,'fontsize',16)
 xlabel('x');
 ylabel('stress change');
